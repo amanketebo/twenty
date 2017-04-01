6a187f3c4f2eef0daca8c6c19aa9e0aa5ae3d02a
amanketebo <amanuelketebo@yahoo.com>
2017-04-01 01:32:59 -0700

    Made StatManager save stats functions safer

--- Twenty/StatsManager.swift
+++ Twenty/StatsManager.swift
@@ -76,26 +76,35 @@ class StatsManager {
             // Limitation of UserDefaults?
             // Get statDictionary from array of stats
             var certainPlayerDict = statsTable[foundPlayerIndex]
-            var totalPoints = certainPlayerDict[certainPlayerDict.keys.first!]?["total points"]
-            var totalFouls = certainPlayerDict[certainPlayerDict.keys.first!]?["total fouls"]
-            var totalTechs = certainPlayerDict[certainPlayerDict.keys.first!]?["total techs"]
-            var gamesWon = certainPlayerDict[certainPlayerDict.keys.first!]?["games won"]
-            var gamesLost = certainPlayerDict[certainPlayerDict.keys.first!]?["games lost"]
+            if let playerName = certainPlayerDict.keys.first {
+                
+                var totalPoints = certainPlayerDict[playerName]?["total points"]
+                var totalFouls = certainPlayerDict[playerName]?["total fouls"]
+                var totalTechs = certainPlayerDict[playerName]?["total techs"]
+                var gamesWon = certainPlayerDict[playerName]?["games won"]
+                var gamesLost = certainPlayerDict[playerName]?["games lost"]
+                
+                guard totalPoints != nil && totalFouls != nil && totalTechs != nil && gamesWon != nil && gamesLost != nil else {
+                    return
+                }
+                
+                // Add stats to the running total
+                totalPoints = totalPoints! + (player[player.keys.first!]?["total points"]!)!
+                totalFouls = totalFouls! + (player[player.keys.first!]?["total fouls"]!)!
+                totalTechs = totalTechs! + (player[player.keys.first!]?["total techs"]!)!
+                gamesWon = gamesWon! + (player[player.keys.first!]?["games won"]!)!
+                gamesLost = gamesLost! + (player[player.keys.first!]?["games lost"]!)!
+                
+                certainPlayerDict[playerName]?["total points"] = totalPoints
+                certainPlayerDict[playerName]?["total fouls"] = totalFouls
+                certainPlayerDict[playerName]?["total techs"] = totalTechs
+                certainPlayerDict[playerName]?["games won"] = gamesWon
+                certainPlayerDict[playerName]?["games lost"] = gamesLost
+                
+                statsTable[foundPlayerIndex] = certainPlayerDict
+                print("We are happy and we know it!")
+            }
             
-            // Add stats to the running total
-            totalPoints = totalPoints! + (player[player.keys.first!]?["total points"]!)!
-            totalFouls = totalFouls! + (player[player.keys.first!]?["total fouls"]!)!
-            totalTechs = totalTechs! + (player[player.keys.first!]?["total techs"]!)!
-            gamesWon = gamesWon! + (player[player.keys.first!]?["games won"]!)!
-            gamesLost = gamesLost! + (player[player.keys.first!]?["games lost"]!)!
-            
-            certainPlayerDict[certainPlayerDict.keys.first!]?["total points"] = totalPoints
-            certainPlayerDict[certainPlayerDict.keys.first!]?["total fouls"] = totalFouls
-            certainPlayerDict[certainPlayerDict.keys.first!]?["total techs"] = totalTechs
-            certainPlayerDict[certainPlayerDict.keys.first!]?["games won"] = gamesWon
-            certainPlayerDict[certainPlayerDict.keys.first!]?["games lost"] = gamesLost
-            
-            statsTable[foundPlayerIndex] = certainPlayerDict
         }
     }
     
@@ -106,15 +115,32 @@ class StatsManager {
         if let allStats = defaults.object(forKey: "allStats") as? [[String:[String:Double]]] {
             for playerStat in allStats {
                 let averageStat = AverageStats()
-                let gamesPlayed = (playerStat[playerStat.keys.first!]?["games won"])! + (playerStat[playerStat.keys.first!]?["games lost"])!
-                averageStat.name = playerStat.keys.first!
-                // Round points, fouls, and techs to the tenths place
-                averageStat.points = ((playerStat[playerStat.keys.first!]?["total points"])! / gamesPlayed).roundTo(places: 1)
-                averageStat.fouls = ((playerStat[playerStat.keys.first!]?["total fouls"])! / gamesPlayed).roundTo(places: 1)
-                averageStat.techs = ((playerStat[playerStat.keys.first!]?["total techs"])! / gamesPlayed).roundTo(places: 1)
-                averageStat.gamesWon = Int((playerStat[playerStat.keys.first!]?["games won"])!)
-                averageStat.gamesLost = Int((playerStat[playerStat.keys.first!]?["games lost"])!)
-                averageStats.append(averageStat)
+                if let playerName = playerStat.keys.first {
+                    let totalPoints = playerStat[playerName]?["total points"]
+                    let totalFouls = playerStat[playerName]?["total fouls"]
+                    let totalTechs = playerStat[playerName]?["total techs"]
+                    let gamesWon = playerStat[playerName]?["games won"]
+                    let gamesLost = playerStat[playerName]?["games lost"]
+                    
+                    guard totalPoints != nil && totalFouls != nil && totalTechs != nil && gamesWon != nil && gamesLost != nil else {
+                        return []
+                    }
+                    
+                    let gamesPlayed = (playerStat[playerName]?["games won"])! + (playerStat[playerName]?["games lost"])!
+                    averageStat.name = playerName
+                    
+                    // Round points, fouls, and techs to the tenths place
+                    averageStat.points = ((totalPoints)! / gamesPlayed).roundTo(places: 1)
+                    averageStat.fouls = ((totalFouls)! / gamesPlayed).roundTo(places: 1)
+                    averageStat.techs = ((totalTechs)! / gamesPlayed).roundTo(places: 1)
+                    averageStat.gamesWon = Int((gamesWon)!)
+                    averageStat.gamesLost = Int((gamesLost)!)
+                    averageStats.append(averageStat)
+                }
+                else {
+                    return []
+                    
+                }
             }
         }
         else {
